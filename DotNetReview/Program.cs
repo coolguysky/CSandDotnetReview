74bdd2a6ebc77b80b26cb34afaf1d1227084a348
Gregg <greggreedonline@gmail.com>
2020-07-13 17:35:34 -0400

    Add project files.

---
+++ DotNetReview/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
